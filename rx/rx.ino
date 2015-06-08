d07ff617bc02faef8bd1a99a9099a2cd73cbbf1e
shakirali2244 <shakirali2244@yahoo.com>
2015-06-08 05:07:28 -0700

    tuning rx

--- rx/rx.ino
+++ rx/rx.ino
@@ -3,12 +3,14 @@
 #include <Servo.h> 
 RH_NRF24 nrf24;
 Servo throttle_pin;
+Servo roll_pin;
+Servo pitch_pin;
+Servo yaw_pin;
 
-String safeVal = "00323232";
+String safeVal = "00505050";
 String payload;
-int pos = 150;
-char mem[2];
-char *ptr;
+String mem;
+//char *ptr;
 
 void setup(){
   Serial.begin(9600);
@@ -21,60 +23,29 @@ void setup(){
     Serial.println("setRF failed");
   Serial.println("all passed");
   throttle_pin.attach(3);
-  throttle_pin.write(30);
-}
-
-
-
-unsigned long hex2int(char *a, unsigned int len)
-{
-  int i;
-  unsigned long val = 0;
-
-  for(i=0;i<len;i++)
-    if(a[i] <= 57)
-      val += (a[i]-48)*(1<<(4*(len-1-i)));
-    else
-      val += (a[i]-55)*(1<<(4*(len-1-i)));
-  return val;
+  roll_pin.attach(5);
+  pitch_pin.attach(6);
 }
 
 void ParseAndWrite(char payload[]){
-  mem[0] = payload[0];
-  mem[1] = payload[1];
-  ptr = mem;
-  unsigned long throttle = hex2int(ptr,2);
-  mem[0] = payload[2];
-  mem[1] = payload[3];
-  ptr = mem;
-  unsigned long roll = hex2int(ptr,2); 
-  mem[0] = payload[4];
-  mem[1] = payload[5];
-  ptr = mem;
-  unsigned long pitch = hex2int(ptr,2);
-  mem[0] = payload[6];
-  mem[1] = payload[7];
-  ptr = mem;
-  unsigned long yaw = hex2int(ptr,2);
-  float throttle_val = (throttle * 255 )/100;
-  float roll_val = (roll * 255 )/100;
-  float pitch_val = (pitch * 255 )/100;
-  float yaw_val = (yaw * 255 )/100;
-  int pos = int(throttle_val);
-  pos = map(pos, 0, 255, 1000, 2000);
-  delay(15);
-  throttle_pin.writeMicroseconds(pos);  
-  //Serial.print(int(throttle_val));
-  //Serial.print(" ");
-  //Serial.print(int(roll_val));
-  //Serial.print(" ");
-  //Serial.print(int(pitch_val));
-  //Serial.print(" ");
-  //Serial.println(int(yaw_val));
-  Serial.println(int(throttle_val));
-  //analogWrite(roll_pin,int(throttle_val));
-  //Serial.println("throttle,roll,pitch,yaw %i %i %i %i ", int(throttle_val) ,int(roll_val) , int(pitch_val) , int(yaw_val));
-}
+  
+   mem="\0";
+   mem+=payload[0];
+   mem+=payload[1];
+   int throttle_val = map( mem.toInt(), 0, 100, 1000, 2000);
+   throttle_pin.writeMicroseconds(throttle_val);
+   mem="\0";
+   mem+=payload[2];
+   mem+=payload[3];
+   int roll_val = map( mem.toInt(), 0, 100, 1000, 2000);
+   roll_pin.writeMicroseconds(roll_val);
+   mem="\0";
+   mem+=payload[4];
+   mem+=payload[5];
+   int pitch_val = map( mem.toInt(), 0, 100, 1000, 2000);
+   pitch_pin.writeMicroseconds(pitch_val);
+   Serial.println(payload);
+  }
   
 void loop(){
   uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
@@ -88,7 +59,7 @@ void loop(){
     //buffer = (char)buf;
     ParseAndWrite(buffer);
   }
-  while(!nrf24.waitAvailableTimeout(30)){
+  while(!nrf24.waitAvailableTimeout(200)){
     payload = safeVal;
     char buffer[11];
     payload.toCharArray(buffer,11);
